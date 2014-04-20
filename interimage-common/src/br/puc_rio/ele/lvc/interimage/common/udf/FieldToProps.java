df879633801efb969c201a7663d81950c2cdcac5
Rodrigo Ferreira <webcsm@gmail.com>
2014-04-20 11:33:29 +0200

    Reorganized folders

--- interimage-common/src/br/puc_rio/ele/lvc/interimage/common/udf/FieldToProps.java
+++ interimage-common/src/br/puc_rio/ele/lvc/interimage/common/udf/FieldToProps.java
@@ -12,44 +12,43 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.*/
 
-package br.puc_rio.ele.lvc.interimage.geometry;
+package br.puc_rio.ele.lvc.interimage.common.udf;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.apache.pig.EvalFunc;
 import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.impl.logicalLayer.schema.Schema;
 
-import com.vividsolutions.jts.geom.Geometry;
-
 /**
- * A UDF that returns the envelope (MBR) of a geometry.
- * Example:
- * 		A = load 'mydata' as (geometry);
- * 		B = foreach A generate Envelope(geometry);
+ * A UDF that updates the properties map with the given field.<br><br>
+ * Example:<br>
+ * 		A = load 'mydata' as (field, props);<br>
+ * 		B = foreach A generate FieldToProps(field, 'name', props) as props;<br>
  * @author Rodrigo Ferreira
  *
  */
-public class Envelope extends EvalFunc<String> {
-	
-	private final GeometryParser geometryParser = new GeometryParser();
+public class FieldToProps extends EvalFunc<Map<String,Object>> {
 	
 	/**
      * Method invoked on every tuple during foreach evaluation.
      * @param input tuple; first column is assumed to have the geometry
      * @exception java.io.IOException
-     * @return envelope (MBR) of the geometry
+     * @return map with the given field
      */
 	@Override
-	public String exec(Tuple input) throws IOException {
-		if (input == null || input.size() == 0)
+	public Map<String,Object> exec(Tuple input) throws IOException {
+		if (input == null || input.size() < 3)
             return null;
         
-		try {			
-			Object objGeometry = input.get(0);
-			Geometry geometry = geometryParser.parseGeometry(objGeometry);
-			return geometry.getEnvelope().toText();
+		try {
+			Object objField = input.get(0);
+			String name = DataType.toString(input.get(1));
+			Map<String,Object> objProperties = DataType.toMap(input.get(2));
+			objProperties.put(name,objField);
+			return objProperties;
 		} catch (Exception e) {
 			throw new IOException("Caught exception processing input row ", e);
 		}
@@ -57,7 +56,7 @@ public class Envelope extends EvalFunc<String> {
 	
 	@Override
     public Schema outputSchema(Schema input) {
-        return new Schema(new Schema.FieldSchema(null, DataType.CHARARRAY));
+        return new Schema(new Schema.FieldSchema(null, DataType.MAP));
     }
 	
 }
