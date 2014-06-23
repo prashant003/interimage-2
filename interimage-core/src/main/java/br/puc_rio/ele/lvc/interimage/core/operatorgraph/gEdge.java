768791efd8b8652362f5941671cca21b97b05764
darioaugusto <darioaugusto@gmail.com>
2014-06-23 20:55:54 -0300

    Update gEdge.java

--- interimage-core/src/main/java/br/puc_rio/ele/lvc/interimage/core/operatorgraph/gEdge.java
+++ interimage-core/src/main/java/br/puc_rio/ele/lvc/interimage/core/operatorgraph/gEdge.java
@@ -1,30 +1,3 @@
-<<<<<<< HEAD:interimage-core/src/main/java/br/puc_rio/ele/lvc/interimage/core/gEdge.java
-package br.puc_rio.ele.lvc.interimage.core;
-
-import org.jgrapht.graph.DefaultEdge;
-
-public class gEdge extends DefaultEdge {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 3311759278952307446L;
-	private boolean activated=true;
-
-
-	public gEdge() {
-	    super();
-	  }
-	
-	public boolean isActivated() {
-		return activated;
-	}
-
-	public void setActivated(boolean activated) {
-		this.activated = activated;
-	}
-	
-}
-=======
 package br.puc_rio.ele.lvc.interimage.core.operatorgraph;
 
 import org.jgrapht.graph.DefaultEdge;
@@ -45,4 +18,3 @@ public class gEdge extends DefaultEdge {
 	}
 	
 }
->>>>>>> upstream/master:interimage-core/src/main/java/br/puc_rio/ele/lvc/interimage/core/operatorgraph/gEdge.java
